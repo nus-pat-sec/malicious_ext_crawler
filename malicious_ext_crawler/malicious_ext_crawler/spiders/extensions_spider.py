c11ede4acdb28783caafbf62aba37b84877143d4
Thanh Tran Vo <t.tranvo@uqconnect.edu.au>
2020-07-17 13:34:49 +1000

    Added meta data to spider except the review.

--- malicious_ext_crawler/malicious_ext_crawler/spiders/extensions_spider.py
+++ malicious_ext_crawler/malicious_ext_crawler/spiders/extensions_spider.py
@@ -20,8 +20,10 @@ class ExtensionsSpider(scrapy.Spider):
 
 
     def start_requests(self):
+        keywords = 'ledger'
+        url_s = 'https://addons.mozilla.org/en-US/firefox/search/?q=%s&type=extension' % keywords
         urls = [
-            'https://addons.mozilla.org/en-US/firefox/search/?q=ledger&type=extension'
+            url_s
             # 'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
@@ -35,49 +37,83 @@ class ExtensionsSpider(scrapy.Spider):
     # parsing the data from pages from the list of url
     def parse(self, response):
         extension_list = []
-
+        extensions = response.css('.SearchResult')
         # test = response.request.meta['driver'].find_elements_by_class_name('SearchResult').text
         # response.request.meta['driver'].title
         # print(response.request.meta['driver'].title)
         # name_extension = response.css('SearchResult::text')
         # page = response.css('.a-na-d-w').extract()
-        extensions = response.request.meta['driver'].find_elements_by_class_name('SearchResult')
+        # extensions = response.request.meta['driver'].find_elements_by_class_name('SearchResult')
         # print(response.request.meta['driver'].title)
         for extension in extensions:
             # Extract metadata of each extensions
-            name = extension.find_element_by_css_selector('.SearchResult-link').text
-
-            text_user_numbers = extension.find_element_by_css_selector('.SearchResult-users-text').text
+            # name = extension.find_element_by_css_selector('.SearchResult-link').text
+            name = extension.css('.SearchResult-link::text').get()
+            text_user_numbers = extension.css('.SearchResult-users-text::text').get()
+            # text_user_numbers = extension.find_element_by_css_selector('.SearchResult-users-text').text
             user_numbers = re.findall("[-+]?\d*\,?\d+|\d+", text_user_numbers)
-
-            text_rating  = extension.find_element_by_css_selector('.visually-hidden').text
+            text_rating = extension.css('.visually-hidden::text').get()
+            # text_rating  = extension.find_element_by_css_selector('.visually-hidden').text
             rating = re.findall("[-+]?\d*\.?\d+|\d+", text_rating)
             
             if len(rating) == 0:
                 rating = [0]
-            
-            text_user_numbers = extension.find_element_by_css_selector('.SearchResult-users-text').text
-            user_numbers = re.findall("[-+]?\d*\,?\d+|\d+", text_user_numbers)
 
-            ext_item = {
-                'name': name,
-                'user_numbers': user_numbers[0],
-                'rating': rating[0]
-            }
-            extension_list.append(ext_item)
+            creator = extension.css('h3.SearchResult-author.SearchResult--meta-section::text').get()
+            
+            # text_user_numbers = extension.find_element_by_css_selector('.SearchResult-users-text').text
+            # user_numbers = re.findall("[-+]?\d*\,?\d+|\d+", text_user_numbers)
+            
+            # details = extension.find_element_by_css_selector('.SearchResult-link')
+            # link = details.find_element_by_
+            details_link = extension.css('.SearchResult-link::attr(href)').get()
+            # ext_item = {
+            #     'name': name,
+            #     'user_numbers': user_numbers[0],
+            #     'rating': rating[0],
+            #     'link': details
+            # }
+            if details_link is not None:
+                details_link = response.urljoin(details_link)
+                # yield scrapy.Request(next_page, callback=self.parse)
+                yield scrapy_selenium.SeleniumRequest(url=details_link, callback=self.parse_extension, cb_kwargs={'name':name, 'user_numbers' :user_numbers[0], 'rating' :rating[0], 'creator' :creator})
+            
+
+            # yield {
+            #     'name': name,
+            #     'user_numbers': user_numbers[0],
+            #     'rating': rating[0],
+            #     'details_link': details_link
+            # }
+            # ext_item = {
+            #     'name': name,
+            #     'user_numbers': user_numbers[0],
+            #     'rating': rating[0],
+            #     'details_link': details_link
+            # }
+            # extension_list.append(ext_item)
         # yield {'body': name}
 
 
-        # Create dataframe for it
-        df = pd.DataFrame(extension_list)       
-        # with open('test', 'wb') as f:
-        #     f.write()
-        # # filename = 'test-%s.html' % page
-        # # with open(filename, 'wb') as f:
-        # #     f.write(response.body)
-        # df = pd.DataFrame(test)
-        df.to_csv (r'/Users/thanhtrv/Documents/work/2020/winter_research_2020/malicious_browser_extensions_scrapy/malicious_ext_crawler/malicious_ext_crawler/export_dataframe.csv', index = False, header=True)
+        # CREATE DATAFRAME for storing data
+        # df = pd.DataFrame(extension_list)       
+        # # with open('test', 'wb') as f:
+        # #     f.write()
+        # # # filename = 'test-%s.html' % page
+        # # # with open(filename, 'wb') as f:
+        # # #     f.write(response.body)
+        # # df = pd.DataFrame(test)
+        # df.to_csv (r'/Users/thanhtrv/Documents/work/2020/winter_research_2020/malicious_browser_extensions_scrapy/malicious_ext_crawler/malicious_ext_crawler/export_dataframe.csv', index = False, header=True)
 
 
-
-    
+    def parse_extension(self, response, name, user_numbers, rating, creator):
+        # response.css('title::text').get()
+        
+        yield {
+            'platform': "firefox",
+            'name': name,
+            'rating': rating,
+            'creator': creator,
+            # 'details_link': details_link,
+            'last_updated': response.css('dd.Definition-dd.AddonMoreInfo-last-updated::text').get()
+        }
