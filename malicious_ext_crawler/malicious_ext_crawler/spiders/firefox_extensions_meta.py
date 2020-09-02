1e8e644678fb0b776511bf8c0429ff6be2f5d80f
s4544688 <s4544688@moss.labs.eait.uq.edu.au>
2020-09-02 21:30:09 +1000

    Added code of firefox bot crawler automatically every 8

--- malicious_ext_crawler/malicious_ext_crawler/spiders/firefox_extensions_meta.py
+++ malicious_ext_crawler/malicious_ext_crawler/spiders/firefox_extensions_meta.py
@@ -21,7 +21,7 @@ class FirefoxExtensionsMeta(scrapy.Spider):
         # List of urls for crawling
         urls = []
         # Path to keywords.csv
-        path_keywords_csv = 'malicious_ext_crawler/spiders/full_keyword_2_30.csv'
+        path_keywords_csv = 'malicious_ext_crawler/spiders/input_data/full_medium_keywords.csv'
         # READ and GENERATE urls with keywords 
         with open(path_keywords_csv, mode='r', encoding='utf-8-sig') as csv_file:
             data = csv.reader(csv_file)
